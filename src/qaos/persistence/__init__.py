387cfe5f72fc6edb76311ef2b186417e76827997
Qaasim April <emerge.strategic@gmail.com>
2026-08-03 15:26:53 +0200

    FEAT: Add QAOS SQLite persistence backend

--- src/qaos/persistence/__init__.py
+++ src/qaos/persistence/__init__.py
@@ -5,6 +5,8 @@ QAOS Persistence Package
 from .database import Database
 from .json_store import JSONStore
 
+from .sqlite_store import SQLiteStore
+
 from .registry import register
 from .registry import get
 from .registry import all
@@ -17,6 +19,11 @@ from .manager import (
 register(
     "json",
     JSONStore()
+)
+
+register(
+    "sqlite",
+    SQLiteStore()
 )
 
 __all__ = [
